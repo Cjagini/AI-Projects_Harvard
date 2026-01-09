e04416e06507460252d79095949ecf6f006753c3
Chandrika Jagini <chandrikajagini0905@gmail.com>
2026-01-08 18:29:46 -0600

    Add files via upload

---
+++ puzzle.py
@@ -0,0 +1,98 @@
+from logic import *
+
+AKnight = Symbol("A is a Knight")
+AKnave = Symbol("A is a Knave")
+
+BKnight = Symbol("B is a Knight")
+BKnave = Symbol("B is a Knave")
+
+CKnight = Symbol("C is a Knight")
+CKnave = Symbol("C is a Knave")
+
+# Puzzle 0
+# A says "I am both a knight and a knave."
+knowledge0 = And(
+    # Each person is either a knight or a knave, but not both
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+
+    # A is a knight if and only if "I am both a knight and a knave" is true
+    Biconditional(AKnight, And(AKnight, AKnave))
+)
+
+# Puzzle 1
+# A says "We are both knaves."
+# B says nothing.
+knowledge1 = And(
+    # Structure of the game
+    Or(AKnight, AKnave), Not(And(AKnight, AKnave)),
+    Or(BKnight, BKnave), Not(And(BKnight, BKnave)),
+
+    # A is a knight if and only if "Both A and B are knaves" is true
+    Biconditional(AKnight, And(AKnave, BKnave))
+)
+
+# Puzzle 2
+# A says "We are the same kind."
+# B says "We are of different kinds."
+knowledge2 = And(
+    # Structure of the game
+    Or(AKnight, AKnave), Not(And(AKnight, AKnave)),
+    Or(BKnight, BKnave), Not(And(BKnight, BKnave)),
+
+    # A is a knight if and only if (both are knights OR both are knaves)
+    Biconditional(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+
+    # B is a knight if and only if (one is knight and one is knave)
+    Biconditional(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight)))
+)
+
+# Puzzle 3
+# A says either "I am a knight." or "I am a knave.", but you don't know which.
+# B says "A said 'I am a knave'."
+# B says "C is a knave."
+# C says "A is a knight."
+knowledge3 = And(
+    # Structure of the game
+    Or(AKnight, AKnave), Not(And(AKnight, AKnave)),
+    Or(BKnight, BKnave), Not(And(BKnight, BKnave)),
+    Or(CKnight, CKnave), Not(And(CKnight, CKnave)),
+
+    # A says either "I am a knight" or "I am a knave"
+    Or(
+        Biconditional(AKnight, AKnight),
+        Biconditional(AKnight, AKnave)
+    ),
+
+    # B says "A said 'I am a knave'"
+    # If B is a knight, then what B said is true: (A is a knight <=> A said "I am a knave")
+    Biconditional(BKnight, Biconditional(AKnight, AKnave)),
+
+    # B says "C is a knave."
+    Biconditional(BKnight, CKnave),
+
+    # C says "A is a knight."
+    Biconditional(CKnight, AKnight)
+)
+
+
+def main():
+    symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
+    puzzles = [
+        ("Puzzle 0", knowledge0),
+        ("Puzzle 1", knowledge1),
+        ("Puzzle 2", knowledge2),
+        ("Puzzle 3", knowledge3)
+    ]
+    for puzzle, knowledge in puzzles:
+        print(puzzle)
+        if len(knowledge.conjuncts) == 0:
+            print("    Not yet implemented.")
+        else:
+            for symbol in symbols:
+                if model_check(knowledge, symbol):
+                    print(f"    {symbol}")
+
+
+if __name__ == "__main__":
+    main()
